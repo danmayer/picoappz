4771c7ecf9c5fc850ca2081c69dbc89ac36e573e
Dan Mayer <dan@mayerdan.com>
2013-07-28 13:35:05 -0400

    oops dont execute to detect which it

--- lib/grabzit.rb
+++ lib/grabzit.rb
@@ -2,10 +2,10 @@ class Grabzit
 
   #detects osx or linux and runs
   def self.get_image(site_url, image_location)
-    if `xvfb-run`.match(/not found/)
+    if `which xvfb-run`.match(/not found/)
       `webkit2png #{site_url} -o #{image_location}`
     else
-      `xvfb-run -a -s "-screen 0 1024x768x16" webkit2png #{site_url} -o #{image_location}`
+      `xvfb-run -a -s "-screen 0 1024x768x16" "webkit2png #{site_url} -o #{image_location}"`
     end
   end
 
